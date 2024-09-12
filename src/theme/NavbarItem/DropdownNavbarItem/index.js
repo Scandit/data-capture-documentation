1ccdd0889d49fda9e33ce23e66a77beb9438c217
amatvieieva <anna.matvieieva.work@gmail.com>
2024-09-12 09:37:26 +0300

    fixed bug

--- src/theme/NavbarItem/DropdownNavbarItem/index.js
+++ src/theme/NavbarItem/DropdownNavbarItem/index.js
@@ -41,7 +41,7 @@ function DropdownNavbarItemDesktop({
   const [link, setLink] = useState("/add-sdk");
   const location = useLocation();
   const currentPath = location.pathname;
-  
+
   useEffect(() => {
     const handleClickOutside = (event) => {
       if (!dropdownRef.current || dropdownRef.current.contains(event.target)) {
@@ -71,7 +71,10 @@ function DropdownNavbarItemDesktop({
         continue;
       }
 
-      setLink(trimmedPath || '/add-sdk');
+      if (trimmedPath.endsWith("/")) {
+        trimmedPath = trimmedPath.slice(0, -1);
+      }
+      setLink(trimmedPath || "/add-sdk");
     }
   }, [currentPath]);
 
