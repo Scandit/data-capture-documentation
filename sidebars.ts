1b38da35e8da351c96c889b90ce7cf4f99a7a8fb
Gabriel <83644514+gpene@users.noreply.github.com>
2025-01-28 10:43:29 +0100

    Update sidebars.ts

--- sidebars.ts
+++ sidebars.ts
@@ -162,6 +162,13 @@ const sidebars: SidebarsConfig = {
             },
             {
               type: "category",
+              label: "MatrixScan Check",
+              items: [
+                "sdks/ios/matrixscan-check/intro",
+              ],
+            },
+            {
+              type: "category",
               label: "MatrixScan Count",
               items: [
                 "sdks/ios/matrixscan-count/intro",
@@ -548,6 +555,13 @@ const sidebars: SidebarsConfig = {
             },
             {
               type: "category",
+              label: "MatrixScan Check",
+              items: [
+                "sdks/web/matrixscan-check/intro",
+              ],
+            },
+            {
+              type: "category",
               label: "MatrixScan Find",
               items: [
                 "sdks/web/matrixscan-find/intro",
@@ -871,6 +885,13 @@ const sidebars: SidebarsConfig = {
               ],
             },
             {
+              type: "category",
+              label: "MatrixScan Check",
+              items: [
+                "sdks/react-native/matrixscan-check/intro",
+              ],
+            },
+            {
               type: 'category',
               label: 'MatrixScan Count',
               items: [
@@ -1052,6 +1073,13 @@ const sidebars: SidebarsConfig = {
                 "sdks/flutter/matrixscan/intro",
                 "sdks/flutter/matrixscan/get-started",
                 "sdks/flutter/matrixscan/advanced",
+              ],
+            },
+            {
+              type: "category",
+              label: "MatrixScan Check",
+              items: [
+                "sdks/flutter/matrixscan-check/intro",
               ],
             },
             {
