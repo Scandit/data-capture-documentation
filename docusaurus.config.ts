716eaca83aad719f38244448964a8fc0a9167baa
Gabriel <83644514+gpene@users.noreply.github.com>
2025-06-10 14:17:04 +0200

    Id bolt 1.9.0 api (#200)
    * added ID-Bolt 1.8.0 API
    
    * Added id-bolt 1.9.0 api
    
    * release notes and versions
    
    ---------
    
    Co-authored-by: Thomas Debrunner <thomas.debrunner@scandit.com>

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -226,6 +226,7 @@ const config: Config = {
             },
             '7.3.0': {
               banner: 'none',
+              badge: false,
             },
             '6.28.5': {
               banner: 'none',
