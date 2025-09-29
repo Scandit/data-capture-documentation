69d3a6c129049f68a1822a101a34fafb489c9b59
Gabriel <83644514+gpene@users.noreply.github.com>
2025-09-29 16:17:13 +0200

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -440,7 +440,7 @@ const config: Config = {
            ],
          },
        ],
-      copyright: `Copyright © ${new Date().getFullYear()} Scandit`,
+      copyright: `Copyright © ${new Date().getFullYear()} Scandit<br>Scandit's products are protected under the patents listed at <a href="https://www.scandit.com/patents/" target="_blank" rel="noopener noreferrer">scandit.com/patents</a>`,
     },
     prism: {
       theme: prismThemes.github,
