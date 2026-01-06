3ebd08fcec620f44480d0be2aa3cd550ef25da90
Moritz Hartmeier <moritz@scandit.com>
2026-01-06 17:53:14 +0100

    Remove Barcode Selection from Sidebar

--- sidebars.ts
+++ sidebars.ts
@@ -195,15 +195,6 @@ const sidebars: SidebarsConfig = {
                 "sdks/ios/barcode-capture/get-started-with-swift-ui",
               ],
             },
-            {
-              type: "category",
-              label: "Barcode Selection",
-              items: [
-                "sdks/ios/barcode-selection/intro",
-                "sdks/ios/barcode-selection/get-started",
-                "sdks/ios/barcode-selection/get-started-with-swift-ui",
-              ],
-            },
           ],
         },
         {
@@ -427,14 +418,6 @@ const sidebars: SidebarsConfig = {
               label: 'Barcode Capture API',
               items: [
                 "sdks/android/barcode-capture/get-started",
-              ],
-            },
-            {
-              type: "category",
-              label: "Barcode Selection",
-              items: [
-                "sdks/android/barcode-selection/intro",
-                "sdks/android/barcode-selection/get-started",
               ],
             },
           ],
@@ -848,14 +831,6 @@ const sidebars: SidebarsConfig = {
                 "sdks/cordova/barcode-capture/get-started",
               ],
             },
-            {
-              type: 'category',
-              label: 'Barcode Selection',
-              items: [
-                "sdks/cordova/barcode-selection/intro",
-                "sdks/cordova/barcode-selection/get-started",
-              ],
-            },
           ],
         },
         {
@@ -1044,14 +1019,6 @@ const sidebars: SidebarsConfig = {
               label: 'Barcode Capture API',
               items: [
                 "sdks/react-native/barcode-capture/get-started",
-              ],
-            },
-            {
-              type: 'category',
-              label: 'Barcode Selection',
-              items: [
-                "sdks/react-native/barcode-selection/intro",
-                "sdks/react-native/barcode-selection/get-started",
               ],
             },
           ],
@@ -1272,14 +1239,6 @@ const sidebars: SidebarsConfig = {
                 "sdks/flutter/barcode-capture/get-started",
               ],
             },
-            {
-              type: 'category',
-              label: 'Barcode Selection',
-              items: [
-                "sdks/flutter/barcode-selection/intro",
-                "sdks/flutter/barcode-selection/get-started",
-              ],
-            },
           ],
         },
         {
@@ -1489,14 +1448,6 @@ const sidebars: SidebarsConfig = {
               label: 'Barcode Capture API',
               items: [
                 "sdks/capacitor/barcode-capture/get-started",
-              ],
-            },
-            {
-              type: 'category',
-              label: 'Barcode Selection',
-              items: [
-                "sdks/capacitor/barcode-selection/intro",
-                "sdks/capacitor/barcode-selection/get-started",
               ],
             },
           ],
@@ -1782,14 +1733,6 @@ const sidebars: SidebarsConfig = {
                 "sdks/net/ios/barcode-capture/get-started",
               ],
             },
-            {
-              type: 'category',
-              label: 'Barcode Selection',
-              items: [
-                "sdks/net/ios/barcode-selection/intro",
-                "sdks/net/ios/barcode-selection/get-started",
-              ],
-            },
           ],
         },
         {
@@ -1979,14 +1922,6 @@ const sidebars: SidebarsConfig = {
               label: 'Barcode Capture API',
               items: [
                 "sdks/net/android/barcode-capture/get-started",
-              ],
-            },
-            {
-              type: 'category',
-              label: 'Barcode Selection',
-              items: [
-                "sdks/net/android/barcode-selection/intro",
-                "sdks/net/android/barcode-selection/get-started",
               ],
             },
           ],
